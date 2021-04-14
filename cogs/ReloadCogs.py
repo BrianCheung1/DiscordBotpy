381ac4923175d85a4abc587313ee21690d429793
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-04-14 19:22:44 -0400

    added admin permissions

--- cogs/ReloadCogs.py
+++ cogs/ReloadCogs.py
@@ -6,15 +6,16 @@ import os
 import sys
 import json
 
+
 class ReloadCogs(commands.Cog):
     "Reloads cogs if changes are made"
 
     def __init__(self, bot, *args, **kwargs):
         self.bot = bot
 
-
+    @commands.has_permissions(administrator=True)
     @commands.command()
-    #reloads all the extensions in the list
+    # reloads all the extensions in the list
     async def reload(self, ctx):
         if not ctx.author.guild_permissions.administrator:
             return
@@ -24,25 +25,28 @@ class ReloadCogs(commands.Cog):
                 self.bot.reload_extension(f'cogs.{filename[:-3]}')
         await ctx.channel.send('Cogs Reloaded')
 
+    @commands.has_permissions(administrator=True)
     @commands.command()
-    #loads extensions manually 
-    async def load(self, ctx ,extension):
+    # loads extensions manually
+    async def load(self, ctx, extension):
         try:
             self.bot.load_extension(f'cogs.{extension}')
             await ctx.channel.send('{} Loaded'.format(extension))
             print('Loaded {}'.format(extension))
         except Exception as error:
             print('{} cannot be loaded. [{}]'.format(extension, error))
-    
+
+    @commands.has_permissions(administrator=True)
     @commands.command()
-    #unloads extensions manually 
-    async def unload(self,ctx,extension):
+    # unloads extensions manually
+    async def unload(self, ctx, extension):
         try:
             self.bot.unload_extension(f'cogs.{extension}')
             await ctx.channel.send('{} unloaded'.format(extension))
             print('Unloaded {}'.format(extension))
         except Exception as error:
             print('{} cannot be unloaded. [{}]'.format(extension, error))
+
 
 def setup(bot):
     bot.add_cog(ReloadCogs(bot))
