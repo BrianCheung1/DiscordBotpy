ed21ec362ad4ccddd316bbc81d7fc2858e1c2f91
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-08-01 19:56:57 -0400

    added games cog

---
+++ cogs/Games.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+
+
+class Games(commands.Cog):
+    """Makes Display For Games Look Better"""
+
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+
+    @commands.command()
+    async def game(self, ctx, game_title, steam_link, direct_download):
+        """Displays games in embed"""
+        
+        embed = discord.Embed(
+            title=f"Google Sheets Updated with {game_title}", color=ctx.author.color)
+        embed.add_field(name="Steam Link: ", value=steam_link, inline=False)
+        embed.add_field(name="Direct Download: ",
+                        value=direct_download, inline=False)
+        embed.add_field(name="Google Sheets: ",
+                        value="https://docs.google.com/spreadsheets/d/1Qlw02EessdbkTKuPPiXquPdAEtNnUXC1oBYfGk0Y13A/edit#gid=2000315673", inline=False)
+        embed.set_footer(
+            text="Time of Update [" + datetime.now().strftime("%I:%M %p") + "]")
+        await ctx.send(content = None, embed = embed)
+
+
+def setup(bot):
+    bot.add_cog(Games(bot))
+    print('Games is loaded')
