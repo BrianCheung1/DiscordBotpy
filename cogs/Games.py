4a833639c343ffa47a2358d3091439e9d7da0984
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-08-01 20:08:06 -0400

    Update Games.py

--- cogs/Games.py
+++ cogs/Games.py
@@ -9,12 +9,12 @@ class Games(commands.Cog):
     def __init__(self, bot, *args, **kwargs):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(aliases=["games", "ga"])
     async def game(self, ctx, game_title, direct_download, steam_link, notes=None):
         """Displays games in embed"""
-
+        game_title_fixed = game_title.replace("-", " ")
         embed = discord.Embed(
-            title=f"Google Sheets Updated with {game_title}", color=ctx.author.color)
+            title=f"Google Sheets Updated with {game_title_fixed}", color=ctx.author.color)
         embed.add_field(name="Direct Download: ",
                         value=direct_download, inline=False)
         embed.add_field(name="Steam Link: ", value=steam_link, inline=False)
