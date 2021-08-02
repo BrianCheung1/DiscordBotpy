0f86988bb4204ca68e2dea089a66a0778c7835ad
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-08-01 20:04:42 -0400

    Update Games.py

--- cogs/Games.py
+++ cogs/Games.py
@@ -10,19 +10,23 @@ class Games(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def game(self, ctx, game_title, steam_link, direct_download):
+    async def game(self, ctx, game_title, direct_download, steam_link, notes=None):
         """Displays games in embed"""
-        
+
         embed = discord.Embed(
             title=f"Google Sheets Updated with {game_title}", color=ctx.author.color)
-        embed.add_field(name="Steam Link: ", value=steam_link, inline=False)
         embed.add_field(name="Direct Download: ",
                         value=direct_download, inline=False)
+        embed.add_field(name="Steam Link: ", value=steam_link, inline=False)
         embed.add_field(name="Google Sheets: ",
                         value="https://docs.google.com/spreadsheets/d/1Qlw02EessdbkTKuPPiXquPdAEtNnUXC1oBYfGk0Y13A/edit#gid=2000315673", inline=False)
+        notes_fixed = notes.replace("-", "\n")
+
+        embed.add_field(name="Notes: ",
+                        value=notes_fixed, inline=False)
         embed.set_footer(
             text="Time of Update [" + datetime.now().strftime("%I:%M %p") + "]")
-        await ctx.send(content = None, embed = embed)
+        await ctx.send(content=None, embed=embed)
 
 
 def setup(bot):
